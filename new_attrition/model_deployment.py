01986c1c07c97b7c7dda86d0bea3281d64f5b587
Anurup Sankriti <117533355+Anurup-Sankriti@users.noreply.github.com>
2023-06-15 16:41:11 +0530

    Update model_deployment.py

--- new_attrition/model_deployment.py
+++ new_attrition/model_deployment.py
@@ -2,13 +2,17 @@ import pickle
 import pandas as pd
 with open('model.pkl', 'rb') as file:
     loaded_pickle = pickle.load(file)
-recency = input('Enter recency: ') # 0.08 - 28
-no_licenses = input('Enter number of licences: ') # 1 - 8
-no_certificates = input('Enter no. of certificates: ') # 0 - 4
-Age = input('Enter your age: ') # 27 - 84, neg relation
-past_performance=input('Enter past performance rating: ') # 1 - 51
+recency = float(input('Enter recency: '))
+no_licenses = float(input('Enter number of licences: '))
+no_certificates = float(input('Enter no. of certificates: '))
+past_performance=float(input('Enter past performance rating: '))
 
-data = {'recency': [recency], 'no_licenses':[no_licenses], 'no_certificates':[no_certificates], 'Age':[Age], 'past_performance':[past_performance]}
+if recency > 8.8: recency = 8.8
+if no_licenses > 7: no_licenses = 7
+if no_certificates > 3: no_certificates = 3
+if past_performance > 14.375: past_performance = 14.375
+
+data = {'recency': [recency], 'no_licenses':[no_licenses], 'no_certificates':[no_certificates], 'past_performance':[past_performance]}
 new_df = pd.DataFrame(data)
 
 pred = loaded_pickle.predict(new_df)
@@ -16,4 +20,3 @@ if round(pred[0]) == 0:
     print('No attrition')
 else:
     print('Attrition occurs')
-
